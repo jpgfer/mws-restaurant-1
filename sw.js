8d42bd6bf168bb6496882ccaca7759bf08fe70f1
jpgfer <jpgfer@laptop>
2018-04-30 19:07:57 +0100

    feat: Cache implemented for main page static content

--- sw.js
+++ sw.js
@@ -1,17 +1,44 @@
 /**
+ * Add listener to the 'install' event
+ */
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('mws-static-v1')
+    .then(function (cache) {
+      return cache.addAll([
+        '/',
+        'css/styles.css',
+        'js/main.js',
+        'js/dbhelper.js',
+        'data/restaurants.json'
+      ]);
+    })
+    );
+});
+/**
  * Add listener to the 'fetch' event
  */
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    fetch(event.request)
+    // Get the response from cache...
+    caches.match(event.request)
     .then(function (response) {
-      if (response.status === 404) {
-        return new Response('Whoops, not found.');
+      // If there's a cached response...
+      if (response) {
+        // ... return it
+        return response;
       }
-      return response;
-    })
-    .catch(function (error) {
-      return new Response('Error requesting.');
+      // ...else fetch it
+      return fetch(event.request)
+        .then(function (response) {
+          if (response.status === 404) {
+            return new Response('Whoops, not found.');
+          }
+          return response;
+        })
+        .catch(function (error) {
+          return new Response('Error requesting.');
+        });
     })
     );
 });
