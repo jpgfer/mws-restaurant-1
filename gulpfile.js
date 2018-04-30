079c249191d61854da426cae6ac3e337a4e4c3c5
jpgfer <jpgfer@laptop>
2018-04-30 11:02:51 +0100

    chore: Setup sass to css conversion task (no scss available yet)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', defaultTask);
+// Default task
+gulp.task('default', function () {
+  // Watch for changes in sass files
+  gulp.watch('sass/**/*.scss',['styles']);
+});
 
-function defaultTask(done) {
-  console.log('Hello world!');
-  done();
-}
+// Sass to CSS task
+gulp.task('styles', function () {
+  // TODO: For now this task won't be used (postponed to project part 2 or 3)
+  return gulp.src('sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./css'));
+});
+
+
