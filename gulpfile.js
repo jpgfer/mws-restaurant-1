84a32e24eff93f9383b5f59750f3089d95e934ff
jpgfer <jpgfer@laptop>
2018-04-30 12:02:30 +0100

    chore: Setup js tasks and 'dist' folder like structure
    1) Added gulp tasks to concat js (usage postponed for later)
    2) Added 'dist' tasks to copy html, css and js files to 'dist' folder
    3) No tasks for the resources have been done yet (data and images manually copied)

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,75 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
 
 // Default task
 gulp.task('default', function () {
-  // Watch for changes in sass files
-  gulp.watch('sass/**/*.scss',['styles']);
+  // Watch for changes in files
+  gulp.watch('*.html', ['dist-html']);
+  gulp.watch('css/**/*.css', ['dist-css']);
+  gulp.watch('js/**/*.js', ['dist-js']);
 });
 
-// Sass to CSS task
+/**
+ * HTML tasks
+ */
+// Copy html files to dist folder
+gulp.task('dist-html', function () {
+  return gulp
+          .src('*.html')
+          .pipe(gulp.dest('./dist'));
+});
+
+
+/**
+ * CSS tasks
+ */
+// Copy css to dist folder
+gulp.task('dist-css', function () {
+  return gulp
+          .src('css/**/*.css')
+          .pipe(gulp.dest('./dist/css'));
+});
+// TODO: For now this task won't be used (postponed to project part 2 or 3)
 gulp.task('styles', function () {
-  // TODO: For now this task won't be used (postponed to project part 2 or 3)
   return gulp.src('sass/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(autoprefixer({
-        browsers: ['last 2 versions']
-      }))
-      .pipe(gulp.dest('./css'));
+          .pipe(sass().on('error', sass.logError))
+          .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+          }))
+          .pipe(gulp.dest('./css'));
 });
 
+/**
+ * JS tasks 
+ * TODO: setup production tasks and use ".pipe(uglify())" before  ".pipe(gulp.dest('js'))"
+ */
+// Copy scripts to dist folder
+gulp.task('dist-js', function () {
+  return gulp
+          .src('js/**/*.js')
+          .pipe(gulp.dest('./dist/js'));
+});
+
+// Concatenate main page js
+// TODO: For now this task won't be used (postponed to project part 2 or 3)
+gulp.task('scripts-main', function () {
+  return gulp.src(['js/main.js', 'js/dbhelper.js'])
+          .pipe(concat('main_page.js'))
+          .pipe(gulp.dest('js'));
+});
+
+// Concatenate restaurant info page js
+// TODO: For now this task won't be used (postponed to project part 2 or 3)
+gulp.task('scripts-info', function () {
+  return gulp.src(['js/restaurant_info.js', 'js/dbhelper.js'])
+          .pipe(concat('info_page.js'))
+          .pipe(gulp.dest('js'));
+});
+
+/**
+ * DIST task
+ */
+gulp.task('dist', gulp.parallel('dist-html', 'dist-css', 'dist-js'));
 
