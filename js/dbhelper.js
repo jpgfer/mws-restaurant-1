33e07b7f2ea7dce64413e373452eb8e53cda2d75
jpgfer <jpgfer@laptop>
2018-06-01 16:27:38 +0100

    feat: Json data from server is cached in the Indexed DB
    Pending issues:
    1) Restaurant data is requested 3 times in main page
    2) Improve serviceworker performance for restaurant details, ie, cache 
    only the skeleton ("restaurant.html") and use it for different id's

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,3 +1,16 @@
+/* global indexedDB */
+
+/**
+ * The restaurants Indexed DB name
+ * @type String
+ */
+const RESTAURANT_DB = 'restaurants-db';
+/**
+ * The restaurants store name
+ * @type String
+ */
+const RESTAURANT_STORE = 'restaurant';
+
 /**
  * Common database helper functions.
  */
@@ -5,11 +18,8 @@ class DBHelper {
 
   /**
    * Database URL.
-   * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-//    const port = 8383 // Change this to your server port
-//    return `http://localhost:${port}/data/restaurants.json`;
     return `http://localhost:1337/restaurants`;
   }
 
@@ -21,32 +31,70 @@ class DBHelper {
   static fetchRestaurantsData(restaurantsDataUrl, callback) {
     fetch(restaurantsDataUrl)
       .then(function (response) {
-        if (response.status === 200) {// Got a success response from server!
+        // If response is OK...
+        if (response.status === 200) {
+          // ... return the json promise
           return response.json();
-        } else {// Oops!. Got an error from server.
-          const error = (`Request failed. Returned status of ${response.status}`);
-          callback(error, null);
+        } else {
+          // ... else throw an error to be handled in the catch
+          throw new Error(`Fetch failed: ${response.status} ${response.statusText}`);
         }
-      })      
-      .then(function(restaurants){
+      })
+      .then(function (restaurants) {
+        // Signal back the restaurants data
         callback(null, restaurants);
       })
-      .catch(function(error){
+      .catch(function (error) {
+        // Signal back a fetch error
         callback(error, null);
       });
   }
+
   /**
-   * Fetch all restaurants.
+   * @description Get all restaurants, checking first if they are available offline. 
+   * If not, data is fetched from server and stored in indexed DB.
+   * @param {function(string,json)} callback to handle the error/restaurant data
    */
-  static fetchRestaurants(callback) {
-    DBHelper.fetchRestaurantsData(DBHelper.DATABASE_URL, callback);
+  static getRestaurants(callback) {
+    DBHelper.selectRestaurantsData((error, restaurants) => {
+      if (error) {
+        // Log that there was an error retrieving data from db
+        console.info(error);
+      }
+      // If data loaded from database...
+      if (restaurants && restaurants.length > 0) {
+        // ... pass it to the callback
+        callback(null, restaurants);
+      } else {
+        // ... else fetch it and insert into db
+        console.info(`No restaurant data in database. Fetching from server...`);
+        DBHelper.fetchAndInsertRestaurantData(DBHelper.DATABASE_URL, callback);
+      }
+    });
   }
 
   /**
-   * Fetch a restaurant by its ID.
+   * @description Get a restaurant by its ID, checking first if they are available 
+   * offline. If not, data is fetched from server and stored in indexed DB.
+   * @param {string} id
+   * @param {function(string,json)} callback to handle the error/restaurant data
    */
   static fetchRestaurantById(id, callback) {
-    DBHelper.fetchRestaurantsData(`${DBHelper.DATABASE_URL}/${id}`, callback);
+    DBHelper.selectRestaurantsData((error, restaurant) => {
+      if (error) {
+        // Log that there was an error retrieving data from db
+        console.info(error);
+      }
+      // If data loaded from database...
+      if (restaurant) {
+        // ... pass it to the callback
+        callback(null, restaurant);
+      } else {
+        // ... else fetch it and insert into db
+        console.info(`No restaurant data in database for id=${id}. Fetching from server...`);
+        DBHelper.fetchAndInsertRestaurantData(`${DBHelper.DATABASE_URL}/${id}`, callback);
+      }
+    }, +id);  // +id --> converts the id string to a number
   }
 
   /**
@@ -54,7 +102,7 @@ class DBHelper {
    */
   static fetchRestaurantByCuisine(cuisine, callback) {
     // Fetch all restaurants  with proper error handling
-    DBHelper.fetchRestaurants((error, restaurants) => {
+    DBHelper.getRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
@@ -70,7 +118,7 @@ class DBHelper {
    */
   static fetchRestaurantByNeighborhood(neighborhood, callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
+    DBHelper.getRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
@@ -86,7 +134,7 @@ class DBHelper {
    */
   static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
+    DBHelper.getRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
@@ -107,7 +155,7 @@ class DBHelper {
    */
   static fetchNeighborhoods(callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
+    DBHelper.getRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
@@ -125,7 +173,7 @@ class DBHelper {
    */
   static fetchCuisines(callback) {
     // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
+    DBHelper.getRestaurants((error, restaurants) => {
       if (error) {
         callback(error, null);
       } else {
@@ -164,6 +212,128 @@ class DBHelper {
       animation: google.maps.Animation.DROP}
     );
     return marker;
+  }
+
+  /**
+   * @description Do a given operation in the database
+   * @param {string} mode transaction mode
+   * @param {function(transaction)} doInTransaction
+   * @param {function} onSuccessCallback
+   * @param {function} onErrorCallback
+   */
+  static doInDatabase(mode, doInTransaction, onSuccessCallback, onErrorCallback) {
+    const db = indexedDB.open(RESTAURANT_DB, 1);
+    db.onupgradeneeded = (upgradeEvent) => {
+      const upgradeDB = upgradeEvent.target.result;
+      if (!upgradeDB.objectStoreNames.contains(RESTAURANT_STORE)) {
+        upgradeDB.createObjectStore(RESTAURANT_STORE, {keyPath: 'id'});
+      }
+    };
+    if (onErrorCallback) {
+      db.onerror = (errorEvent) => {
+        onErrorCallback(`Error opening database: ${errorEvent.type}`);
+      };
+    }
+    db.onsuccess = (successEvent) => {
+      DBHelper.doInTransaction(successEvent.target.result, [RESTAURANT_STORE], mode, doInTransaction, onSuccessCallback, onErrorCallback);
+    };
+  }
+
+  /**
+   * @description Do a given operation in a transaction
+   * @param {IDBDatabase} db
+   * @param {[string]} storeArray
+   * @param {string} mode
+   * @param {function(IDBTransaction)} doInTransaction
+   * @param {function(string)} onSuccessCallback
+   * @param {function(string)} onErrorCallback
+   */
+  static doInTransaction(db, storeArray, mode, doInTransaction, onSuccessCallback, onErrorCallback) {
+    const transaction = db.transaction(storeArray, mode);
+    if (onSuccessCallback) {
+      transaction.oncomplete = (completeEvent) => {
+        onSuccessCallback('Transaction successfull.');
+      };
+    }
+    if (onErrorCallback) {
+      transaction.onerror = (errorEvent) => {
+        onErrorCallback(`Error during transaction.`);
+      };
+      transaction.onabort = (abortEvent) => {
+        onErrorCallback(`Transaction aborted.`);
+      };
+    }
+    // Do database operations
+    doInTransaction(transaction);
+  }
+
+  /**
+   * @description Get restaurant's data from DB
+   * @param {function} callback function with 2 parameters: error string and restaurant data json 
+   * @param {string} id (optional) restaurant's ID or undefined to fetch all restaurants
+   */
+  static selectRestaurantsData(callback, id) {
+    console.info(`Loading restaurant data from database.`);
+    DBHelper.doInDatabase('readonly',
+      (transaction) => {
+      // Get the store
+      const store = transaction.objectStore(RESTAURANT_STORE);
+      const withoutId = id === undefined || id === null;
+      // Do select
+      const request = withoutId ? store.getAll() : store.get(id);
+      request.onerror = (errorEvent) => {
+        callback(withoutId ? `Error selecting all restaurants: ${errorEvent}` : `Error selecting restaurant with id=${id}: ${errorEvent}`, null);
+      };
+      request.onsuccess = (successEvent) => {
+        callback(null, successEvent.target.result);
+      };
+    }, (successMessage) => {
+      console.info('Restaurant data loaded from database.');
+    }, (errorMessage) => {
+      console.info(`Error loading restaurant data from database: ${errorMessage}`);
+    }
+    );
+
+  }
+
+  /**
+   * @description Intercepts the fetchRestaurantsData callback to update indexed DB content
+   * @param {type} callback
+   * @returns {Function}
+   */
+  static insertRestaurantData(restaurantData) {
+    console.info(`Inserting restaurant data into database.`);
+    DBHelper.doInDatabase('readwrite',
+      (transaction) => {
+      const store = transaction.objectStore(RESTAURANT_STORE);
+      const insertRestaurant = function (restaurant) {
+        const request = store.put(restaurant);
+        request.onsuccess = function (event) {
+          console.info(event);// event.target.result === customer.ssn;
+        };
+      };
+      if (Array.isArray(restaurantData)) {
+        restaurantData.forEach(insertRestaurant);
+      } else {
+        insertRestaurant(restaurantData);
+      }
+    }, (successMessage) => {
+      console.info('Restaurant data inserted.');
+    }, (errorMessage) => {
+      console.info(`Error inserting restaurant data: ${errorMessage}`);
+    }
+    );
+  }
+
+  static fetchAndInsertRestaurantData(restaurantsDataUrl, callback) {
+    DBHelper.fetchRestaurantsData(restaurantsDataUrl, (error, restaurants) => {
+      if (error) {
+        console.info(`Error fetching restaurant data from ${restaurantsDataUrl}.`);
+      } else {
+        DBHelper.insertRestaurantData(restaurants);
+      }
+      callback(error, restaurants);
+    });
   }
 
 }
